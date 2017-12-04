423789e8a6707776a94e93e45e817b30ce3d8c73
Alejandro Romero <romero.m.alejandro@gmail.com>
2017-12-03 21:40:26 -0300

    Several changes

--- files/modules/customer_national_order/payment.php
+++ files/modules/customer_national_order/payment.php
@@ -36,19 +36,19 @@
                     <h4 class="subTitleB"><i class="fa fa-cubes"></i> Productos</h4>
                     <div style="margin:0px 10px;">
                         <div class="row form-group inline-form-custom bg-red" style="margin-bottom:0px!important;">
-                            <div class="col-xs-3 txC">
+                            <div class="col-sm-4 col-xs-12 txC">
                                 <strong>Art&iacute;culo</strong>
                             </div>
-                            <div class="col-xs-2 txC">
+                            <div class="col-sm-2 col-xs-6 txC">
                                 <strong>Precio</strong>
                             </div>
-                                <div class="col-xs-2 txC">
+                                <div class="col-sm-2 col-xs-6 txC">
                             <strong>Cantidad</strong>
                             </div>
-                            <div class="col-xs-2 txC">
+                            <div class="col-sm-1 col-xs-6 txC">
                                 <strong>Costo</strong>
                             </div>
-                            <div class="col-xs-3 txC">
+                            <div class="col-sm-2 col-xs-6 txC">
                                 <strong>Acciones</strong>
                             </div>
                         </div>
@@ -60,20 +60,20 @@
                             <?php $DecimalKgs = strtolower($Item['size'])=='kgs'? '.99':''; ?>
                             <div id="item_row_<?php echo $I ?>" item="<?php echo $I ?>" class="row form-group inline-form-custom ItemRow bg-gray" style="margin-bottom:0px!important;padding:10px 0px!important;">
                                 <form id="item_form_<?php echo $I ?>" name="item_form_<?php echo $I ?>">
-                                    <div class="col-xs-3 txC">
+                                    <div class="col-sm-4 col-xs-12 txC">
                                         <span id="Item<?php echo $I ?>" class="ItemText<?php echo $I ?>"><i class="fa fa-cube"></i> <?php echo $Item['title'] ?></span>
                                         <?php echo insertElement('hidden','item_'.$I,$Item['item_id']);?>
                                     </div>
-                                    <div class="col-xs-2 txC">
+                                    <div class="col-sm-2 col-xs-6 txC">
                                         $ <span id="PricePayment<?php echo $I ?>" class="ItemText<?php echo $I ?>"><?php echo $Item['price'] ?></span>
                                         <?php echo insertElement('hidden','price_'.$I,$Item['price']);?>
                                     </div>
-                                    <div class="col-xs-2 txC">
+                                    <div class="col-sm-2 col-xs-6 txC">
                                         <span id="QuantityPayment<?php echo $I ?>" class="Hidden ItemText<?php echo $I ?>"><?php echo $Item['quantity'].' '.$Item['size'] ?></span>
-                                        <?php echo insertElement('text','quantity_'.$I,$Item['quantity'],'ItemField'.$I.' form-control calcablePayment QuantityItemPayment txC','data-inputmask="\'mask\': \'9{+}'.$DecimalKgs.'\'" placeholder="Cantidad" validateEmpty="Ingrese una cantidad" style="max-width:50%!important;display:inline-block;"').' '.$Item['size']; ?>
+                                        <?php echo insertElement('text','quantity_'.$I,$Item['quantity'],'ItemField'.$I.' form-control calcablePayment QuantityItemPayment txC','data-inputmask="\'mask\': \'9{+}'.$DecimalKgs.'\'" placeholder="Cantidad" validateEmpty="Ingrese una cantidad" style="max-width:70%!important;display:inline-block;"').' '.$Item['size']; ?>
                                     </div>
-                                    <div id="item_number_<?php echo $I ?>" class="col-xs-2 txC item_number" total="<?php echo $Item['total']; ?>" item="<?php echo $I ?>">$ <?php echo $Item['total']; ?></div>
-                                    <div class="col-xs-3 txC">
+                                    <div id="item_number_<?php echo $I ?>" class="col-sm-1 col-xs-6 txC item_number" total="<?php echo $Item['total']; ?>" item="<?php echo $I ?>">$ <?php echo $Item['total']; ?></div>
+                                    <div class="col-sm-2 col-xs-6 txC">
                                         <button type="button" id="SaveItemPayment<?php echo $I ?>" title="Entregar" class="btn btnGreen SaveItemPayment" style="margin:0px;" item="<?php echo $I ?>"><i class="fa fa-check"></i></button>
                                         <button type="button" id="EditItemPayment<?php echo $I ?>" class="btn btnBlue EditItemPayment Hidden" style="margin:0px;" item="<?php echo $I ?>"><i class="fa fa-pencil"></i></button>
                                         <button type="button" id="DeleteItemPayment<?php echo $I ?>"  title="No puede ser entregado" class="btn btnRed DeleteItemPayment" style="margin:0px;" item="<?php echo $I ?>"><i class="fa fa-times"></i></button>	  
