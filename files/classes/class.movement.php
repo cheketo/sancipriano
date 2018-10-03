79a9e709afa5ffab17e1d77e3f96e9ad14a9ca20
Alejandro Romero <romero.m.alejandro@gmail.com>
2018-10-03 11:10:50 -0300

    Add credit movement to customer bug fixed

--- files/classes/class.movement.php
+++ files/classes/class.movement.php
@@ -7,37 +7,39 @@
             $DB->Connect();
             return $DB;
         }
-        
+
         public static function GetMovementsByCustomerID($ID)
         {
             $DB = self::Connect();
             return $DB->fetchAssoc('movement a INNER JOIN movement_type b ON (a.type_id=b.type_id) LEFT JOIN payment c ON (c.payment_id=a.payment_id)','a.*,b.title AS type,b.operation,c.title AS payment','customer_id='.$ID,'a.creation_date DESC,a.type_id');
         }
-        
+
         public static function InsertMovement($Amount=0,$CustomerID,$TypeID,$Concept="",$OrderID=0,$Status="P",$PaymentID=0,$ParentID=0,$CheckID=0)
         {
             $DB = self::Connect();
             if(!$Concept)
             {
-                
+
             }
-            
+
             $Customer = $DB->fetchAssoc("customer","*","customer_id=".$CustomerID);
-    		$Customer = $Customer[0];
-    		$Balance = $Customer['balance'];
-    		
-    		$Type = $DB->fetchAssoc("movement_type","*","type_id=".$TypeID);
-    		if($Type[0]['operation']=="C")
-    		    $Balance += $Amount;
-    		else
-    		    $Balance -= $Amount;
-    		
+    		    $Customer = $Customer[0];
+    		    $Balance = $Customer['balance'];
+
+        		$Type = $DB->fetchAssoc("movement_type","*","type_id=".$TypeID);
+        		if($Type[0]['operation']=="C")
+            {
+        		    $Balance = floatval($Balance) + floatval($Amount);
+        		}else{
+        		    $Balance = floatval($Balance) + floatval($Amount);
+            }
+
             $DB->execQuery('UPDATE','customer','balance='.$Balance,"customer_id=".$CustomerID);
             $DB->execQuery('INSERT','movement','customer_id,type_id,order_id,payment_id,parent_id,check_id,amount,balance,concept,status,creation_date,created_by',$CustomerID.",".$TypeID.",".$OrderID.",".$PaymentID.",".$ParentID.",".$CheckID.",".$Amount.",".$Balance.",'".$Concept."','".$Status."',NOW(),".$_SESSION['admin_id']);
             //echo $DB->lastQuery();
             return $DB->GetInsertId();
         }
-        
+
         public static function UpdateMovement($MovementID,$Amount=0,$CustomerID=0,$TypeID=0,$Concept="",$OrderID=0,$PaymentID=0,$Status="")
         {
             $DB = self::Connect();
@@ -50,7 +52,7 @@
             if($AmountD>0) $Data['amount'] = $Amount;
             if($Concept!="") $Data['concept'] = $Concept;
             if($Status!="") $Data['status'] = $Status;
-            
+
             $DB->execQuery('UPDATE','movement',
                 "customer_id='.$CustomerID.',
                 type_id=".$TypeID.",
@@ -62,7 +64,7 @@
                 updated_by=".$_SESSION['admin_id'],
                 "movement_id=".$MovementID);
         }
-        
+
         public static function UpdateMovementByOrderID($Amount=0,$CustomerID=0,$TypeID=0,$Concept="",$OrderID)
         {
             $DB = self::Connect();
@@ -72,7 +74,7 @@
                 $Data = $Data[0];
                 if($CustomerID>0) $Data['customer_id'] = $CustomerID;
                 if($AmountD>0) $Data['amount'] = $Amount;
-                
+
                 $DB->execQuery('UPDATE','movement',
                     "customer_id='.$CustomerID.',
                     amount=".$Amount.",
@@ -82,19 +84,19 @@
                 self::InsertMovement($Amount,$CustomerID,$TypeID,$Concept,$OrderID);
             }
         }
-        
+
         public function DeleteMovement($MovementID)
         {
             $DB = self::Connect();
             $DB->execQuery('UPDATE','movement',"status='I'",'movement_id='.$MovementID);
         }
-        
+
         public function DeleteOrdersMovements($ID)
         {
             $DB = self::Connect();
             $DB->execQuery('UPDATE','movement',"status='I'",'order_id='.$ID);
         }
-        
+
         public function ActivateOrdersMovements($ID)
         {
             $DB = self::Connect();
